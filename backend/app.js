f4668954254c743387a35c821acb5e143b34aa11
vstorresti <vstorresti@gmail.com>
2020-11-13 12:01:54 -0300

    Adding HTTP Requests and CORS

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,38 @@ const express = require("express");
 
 const app = express();
 
+// Config CORS
 app.use((req, res, next) => {
-  console.log("First middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Origin, X-Request-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "1",
+      title: "First server-side post",
+      content: "this is coming from the server",
+    },
+    {
+      id: "2",
+      title: "First server-side post too",
+      content: "this is coming from the server too",
+    },
+  ];
+
+  res.status(200).json({
+    message: "Posts fetched succesfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
