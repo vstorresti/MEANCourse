2d9b0d05abaae869dfdd222aa08588dbcce3fb49
vstorresti <vstorresti@gmail.com>
2020-11-14 01:17:11 -0300

    Adding Mongo & Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
+const { consoleTestResultHandler } = require("tslint/lib/test");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.knq59.mongodb.net/node-angular?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Config CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Request-With, Content-Type, Accept"
   );
   res.setHeader(
@@ -16,23 +35,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "1",
-      title: "First server-side post",
-      content: "this is coming from the server",
-    },
-    {
-      id: "2",
-      title: "First server-side post too",
-      content: "this is coming from the server too",
-    },
-  ];
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
+  res.status(201).json({
+    message: "Post added succesfully!",
+  });
+});
 
-  res.status(200).json({
-    message: "Posts fetched succesfully!",
-    posts: posts,
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched succesfully!",
+      posts: documents,
+    });
   });
 });
 
